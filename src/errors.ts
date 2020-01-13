300060bd89d75d5d93e39ff439cbaf82ee3a67d8
Guido Zuidhof <me@guido.io>
2020-01-13 23:26:13 +0000

    Make some changes

--- src/errors.ts
+++ src/errors.ts
@@ -3,85 +3,85 @@
 // https://github.com/Microsoft/TypeScript/wiki/Breaking-Changes#extending-built-ins-like-error-array-and-map-may-no-longer-work
 
 export class ContainsArrayError extends Error {
-    constructor(path: string) {
-        super(`path ${path} contains an array`);
-        Object.setPrototypeOf(this, ContainsArrayError.prototype);
-    }
+  constructor(path: string) {
+    super(`Path ${path} contains an array`);
+    Object.setPrototypeOf(this, ContainsArrayError.prototype);
+  }
 }
 
 export class ContainsGroupError extends Error {
-    constructor(path: string) {
-        super(`path ${path} contains a group`);
-        Object.setPrototypeOf(this, ContainsGroupError.prototype);
-    }
+  constructor(path: string) {
+    super(`Path ${path} contains a group`);
+    Object.setPrototypeOf(this, ContainsGroupError.prototype);
+  }
 }
 
 export class ArrayNotFoundError extends Error {
-    constructor(path: string) {
-        super(`array not found at path ${path}`);
-        Object.setPrototypeOf(this, ArrayNotFoundError.prototype);
-    }
+  constructor(path: string) {
+    super(`Array not found at path ${path}`);
+    Object.setPrototypeOf(this, ArrayNotFoundError.prototype);
+  }
 }
 
 export class GroupNotFoundError extends Error {
-    constructor(path: string) {
-        super(`ground not found at path ${path}`);
-        Object.setPrototypeOf(this, GroupNotFoundError.prototype);
-    }
+  constructor(path: string) {
+    super(`Ground not found at path ${path}`);
+    Object.setPrototypeOf(this, GroupNotFoundError.prototype);
+  }
 }
 
 export class PathNotFoundError extends Error {
-    constructor(path: string) {
-        super(`nothing not found at path ${path}`);
-        Object.setPrototypeOf(this, PathNotFoundError.prototype);
-    }
+  constructor(path: string) {
+    super(`Nothing not found at path ${path}`);
+    Object.setPrototypeOf(this, PathNotFoundError.prototype);
+  }
 }
 
 export class PermissionError extends Error {
-    constructor(message: string) {
-        super(message);
-        Object.setPrototypeOf(this, PermissionError.prototype);
-    }
+  constructor(message: string) {
+    super(message);
+    Object.setPrototypeOf(this, PermissionError.prototype);
+  }
 }
 
 export class KeyError extends Error {
-    constructor(key: string) {
-        super(`key ${key} not present`);
-        Object.setPrototypeOf(this, KeyError.prototype);
-    }
+  constructor(key: string) {
+    super(`Key ${key} not present`);
+    Object.setPrototypeOf(this, KeyError.prototype);
+  }
 }
 
 export class TooManyIndicesError extends RangeError {
-    constructor(selection: any[], shape: number[]) {
-        super(`too many indices for array; expected ${shape.length}, got ${selection.length}`);
-        Object.setPrototypeOf(this, TooManyIndicesError.prototype);
-    }
+  constructor(selection: any[], shape: number[]) {
+    super(`Too many indices for array; expected ${shape.length}, got ${selection.length}`);
+    Object.setPrototypeOf(this, TooManyIndicesError.prototype);
+  }
 }
 
 export class BoundsCheckError extends RangeError {
-    constructor(dimLength: number) {
-        super(`index out of bounds for dimension with length ${dimLength}`);
-        Object.setPrototypeOf(this, BoundsCheckError.prototype);
-    }
+  constructor(dimLength: number) {
+    super(`Index out of bounds for dimension with length ${dimLength}`);
+    Object.setPrototypeOf(this, BoundsCheckError.prototype);
+  }
 }
 
 export class InvalidSliceError extends RangeError {
-    constructor(from: any, to: any, stepSize: any, reason: any) {
-        super(`slice arguments slice(${from}, ${to}, ${stepSize}) invalid: ${reason}`);
-        Object.setPrototypeOf(this, InvalidSliceError.prototype);
-    }
+  constructor(from: any, to: any, stepSize: any, reason: any) {
+    super(`Slice arguments slice(${from}, ${to}, ${stepSize}) invalid: ${reason}`);
+    Object.setPrototypeOf(this, InvalidSliceError.prototype);
+  }
 }
 
 export class NegativeStepError extends Error {
-    constructor() {
-        super(`Negative step size is not supported when indexing.`);
-        Object.setPrototypeOf(this, NegativeStepError.prototype);
-    }
+  constructor() {
+    super(`Negative step size is not supported when indexing.`);
+    Object.setPrototypeOf(this, NegativeStepError.prototype);
+  }
 }
 
 export class ValueError extends Error {
-    constructor(message: string) {
-        super(message);
-        Object.setPrototypeOf(this, ValueError.prototype);
-    }
+  constructor(message: string) {
+    super(message);
+    Object.setPrototypeOf(this, ValueError.prototype);
+  }
 }
